128d7c0f50c6f4fbb3c02b56468bfa322a5aa366
Haluk YILDIZ <halukyildiz4000@gmail.com>
2020-03-01 19:30:42 +0300

    middleware

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'))
+;
 app.get('/',function (req, res) {
-    res.send("<html><head></head><body>Hello World</body></html>")        
+    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World</h1></body></html>');        
 });
 app.get('/api/:id',function (req,res) {
     res.json({firstname:'haluk',lastname:'yıldız',idisi:req.params.id});
